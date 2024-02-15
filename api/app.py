4c5b93aef842fc9ed214a4588ae2335e6918777b
rayqg22 <72386070+rayqg22@users.noreply.github.com>
2024-02-15 22:12:18 +0000

    Database connection established

--- api/app.py
+++ api/app.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
 
 app = Flask(__name__)
 
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+
+connection_string = f"dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={DB_PORT}"
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    conn = psycopg2.connect(connection_string)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM netflix")
+    shows = cur.fetchall()
 
+    cur.close()
+    conn.close()
+
+    return render_template("index.html", shows=shows)
 
 if __name__ == "__main__":
     app.run(debug=True)
