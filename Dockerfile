dc1aab35a3f26de1d265cdc3661bce48ebd7c6d5
rayqg22 <72386070+rayqg22@users.noreply.github.com>
2024-02-21 14:35:39 +0000

    Docker

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 5001
 
 ENV FLASK_APP=api/app.py
-ENV FLASK_ENV=development
+# ENV FLASK_ENV=development
 
 CMD ["flask", "run", "--host=0.0.0.0", "--port=5001"]
--- api/app.py
+++ api/app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
 import os
 import psycopg2
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv()
 
 app = Flask(__name__)
+
+CORS(app)
 
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
@@ -16,27 +22,31 @@ connection_string = (
 )
 
 
-@app.route('/')
-def index():
+@app.route('/shows')
+def get_shows():
+    country_iso2 = request.args.get('country', default='GB', type=str)
+
     conn = psycopg2.connect(connection_string)
     cur = conn.cursor()
+
     cur.execute("""SELECT DISTINCT show_title
         FROM "netflix" AS s1
-        WHERE country_iso2 = 'GB'
+        WHERE country_iso2 = %s
         AND NOT EXISTS (
-        SELECT 1
-        FROM "netflix" AS s2
-        WHERE s2.show_title = s1.show_title
-        AND s2.country_iso2 != 'GB'
-    );
-    """)
+            SELECT 1
+            FROM "netflix" AS s2
+            WHERE s2.show_title = s1.show_title
+            AND s2.country_iso2 != %s
+        );
+    """, (country_iso2, country_iso2))
+
     shows = cur.fetchall()
 
     cur.close()
     conn.close()
 
-    return render_template("index.html", shows=shows)
-
+    show_titles = [show[0] for show in shows]
+    return jsonify(show_titles)
 
 if __name__ == "__main__":
     app.run(debug=True)
